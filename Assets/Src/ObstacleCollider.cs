4268f63ae93317495ecd17e1823579d59d4a6e77
julian ladjani <julian.ladjani@epitech.eu>
2017-08-26 22:27:12 +0200

    Scene Fix player

---
+++ Assets/Src/ObstacleCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollider: MonoBehaviour {
+
+    public GameObject playerYin;
+    public GameObject playerYang;
+
+    // Use this for initialization
+    void Start () {
+        if (this.GetComponent<SpriteRenderer>().tag == "ObstacleYang")
+        {
+            Debug.Log(this.playerYin.GetComponent<CircleCollider2D>());
+            Physics2D.IgnoreCollision(this.playerYin.GetComponent<CircleCollider2D>(), this.GetComponent<PolygonCollider2D>());
+        }
+        if (this.GetComponent<SpriteRenderer>().tag == "ObstacleYin")
+        {
+            Physics2D.IgnoreCollision(this.playerYang.GetComponent<CircleCollider2D>(), this.GetComponent<PolygonCollider2D>());
+        }
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
