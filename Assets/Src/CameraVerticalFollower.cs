d891471ff23fd5dc3f9ab95738b9edeec6d94ca5
faudil.puttilli <faudil.puttilli@epitech.eu>
2017-08-26 21:53:06 +0200

    Camera focus change on left click

--- Assets/Src/CameraVerticalFollower.cs
+++ Assets/Src/CameraVerticalFollower.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class CameraVerticalFollower : MonoBehaviour {
 
-    public GameObject player;
-
+    public GameObject playerA;
+	public GameObject playerB;
+	private GameObject onFocus;
     public Color colorYin;
     public Color colorYang;
 
@@ -14,22 +15,24 @@ public class CameraVerticalFollower : MonoBehaviour {
 	void Start ()
     {
         this.obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
-
+		this.onFocus = playerA;
         foreach (GameObject obstacle in this.obstacles)
         {
             obstacle.GetComponent<SpriteRenderer>().color = this.colorYin;
         }
 
-        player.GetComponent<SpriteRenderer>().color = this.colorYin;
+        playerA.GetComponent<SpriteRenderer>().color = this.colorYin;
 
         this.GetComponent<Camera>().backgroundColor = this.colorYang;
     }
 	
 	void Update ()
     {
+		if (Input.GetMouseButtonDown(0))
+			this.onFocus = (this.onFocus == this.playerA) ? this.playerB : this.playerA;
         this.transform.position = new Vector3(
             this.transform.position.x,
-            this.player.transform.position.y,
+			this.onFocus.transform.position.y,
             this.transform.position.z
             );
     }
