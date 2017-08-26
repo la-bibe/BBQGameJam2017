f1ea7fe463d2583991486decf88e5ce803f042d5
FantinBibas <fantin.bibas@epitech.eu>
2017-08-26 16:58:27 +0200

    Add basic player controller & Camera & Triggers & Dynamic obstacles

---
+++ Assets/Src/CameraVerticalFollower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraVerticalFollower : MonoBehaviour {
+
+    public GameObject player;
+
+    public Color colorYin;
+    public Color colorYang;
+
+    private GameObject[] obstacles;
+
+	void Start ()
+    {
+        this.obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+
+        foreach (GameObject obstacle in this.obstacles)
+        {
+            obstacle.GetComponent<SpriteRenderer>().color = this.colorYin;
+        }
+
+        player.GetComponent<SpriteRenderer>().color = this.colorYin;
+
+        this.GetComponent<Camera>().backgroundColor = this.colorYang;
+    }
+	
+	void Update ()
+    {
+        this.transform.position = new Vector3(
+            this.transform.position.x,
+            this.player.transform.position.y,
+            this.transform.position.z
+            );
+    }
+}
---
+++ Assets/Src/DynamicObstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicObstacle : MonoBehaviour {
+
+    public float speed;
+    public Vector2 originalPosition;
+    public Vector2 movedPosition;
+
+    public bool moved = false;
+
+    private void Start()
+    {
+        this.transform.position = this.originalPosition;
+    }
+
+    void Update () {
+        if (this.moved)
+        {
+            this.transform.position = this.movedPosition;
+        } else
+        {
+            this.transform.position = this.originalPosition;
+        }
+    }
+}
