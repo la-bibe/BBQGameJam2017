4cb32c3ee086ba1f3e1bf5d198f64e7aad9cf1ad
FantinBibas <fantin.bibas@epitech.eu>
2017-08-26 17:50:33 +0200

    Update dynamic obstacles

--- Assets/Src/DynamicObstacle.cs
+++ Assets/Src/DynamicObstacle.cs
@@ -3,25 +3,102 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DynamicObstacle : MonoBehaviour {
-
     public float speed;
-    public Vector2 originalPosition;
-    public Vector2 movedPosition;
 
-    public bool moved = false;
+    public bool movingObstacle = false;
+    public Vector2 originalPosition;
+    public Vector2 activatedPosition;
+
+    public bool hiddenObstacle = false;
+    public bool originalHidden = false;
+    public bool activatedHidden = false;
+
+
+    public Collider2D objCollider;
+
+    public bool activated = false;
+
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D objRigidbody;
 
     private void Start()
     {
-        this.transform.position = this.originalPosition;
+        if (this.movingObstacle)
+        {
+            this.transform.position = this.originalPosition;
+        }
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.objRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void actualiseHidden()
+    {
+        if (this.hiddenObstacle)
+        {
+            if (this.activated)
+            {
+                this.spriteRenderer.enabled = !this.activatedHidden;
+                this.objCollider.enabled = !this.activatedHidden;
+            }
+            else
+            {
+                this.spriteRenderer.enabled = !this.originalHidden;
+                this.objCollider.enabled = !this.originalHidden;
+            }
+        }
+    }
+
+    public void setActivated(bool activated)
+    {
+        this.activated = activated;
+        this.actualiseHidden();
     }
 
     void Update () {
-        if (this.moved)
+        if (this.movingObstacle)
         {
-            this.transform.position = this.movedPosition;
-        } else
-        {
-            this.transform.position = this.originalPosition;
+            Vector2 velocity = new Vector2(
+                this.speed * Time.deltaTime,
+                this.speed * Time.deltaTime
+                );
+            if (this.activated)
+            {
+                this.spriteRenderer.enabled = !this.activatedHidden;
+                this.objCollider.enabled = !this.activatedHidden;
+                velocity.x *= (this.transform.position.x == this.activatedPosition.x ?
+                    0 :
+                    (this.transform.position.x < this.activatedPosition.x ?
+                    1 :
+                    -1
+                    ));
+                velocity.y *= (this.transform.position.y == this.activatedPosition.y ?
+                    0 :
+                    (this.transform.position.y < this.activatedPosition.y ?
+                    1 :
+                    -1
+                    ));
+
+                this.objRigidbody.velocity = velocity;
+            }
+            else
+            {
+                this.spriteRenderer.enabled = !this.originalHidden;
+                this.objCollider.enabled = !this.originalHidden;
+                velocity.x *= (this.transform.position.x == this.originalPosition.x ?
+                    0 :
+                    (this.transform.position.x < this.originalPosition.x ?
+                    1 :
+                    -1
+                    ));
+                velocity.y *= (this.transform.position.y == this.originalPosition.y ?
+                    0 :
+                    (this.transform.position.y < this.originalPosition.y ?
+                    1 :
+                    -1
+                    ));
+
+                this.objRigidbody.velocity = velocity;
+            }
         }
     }
 }
